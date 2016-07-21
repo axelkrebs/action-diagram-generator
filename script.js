9193df3e18ee71d4bb5d4b4ff96e86e5acb05ea9
yorshelf <axelfecdgs@hotmail.com>
2016-07-21 20:34:39 +0200

    Refactor original module heading token string to regex matching whole line.
    The original module heading string is now a regex.
    The regex matches the whole original module heading line.
    It's a design decision.
    The regex allows to easily :
    - Accounts for line leading blank spaces and tabs
    - Store the remaining text after the heading drawing through parenthese group

--- script.js
+++ script.js
@@ -2,84 +2,114 @@
 // each have properties : heading, footer, border which returns the corresponding formatting strings
 window.onload = updateTranslation;
 
-     function updateTranslation()
-     {
-	 var originalPanel = document.querySelector(".original > textarea");
-	 var originalText = originalPanel.value;
-	 var translationPanel = document.querySelector(".translation > textarea");
-	 translationPanel.value = actionDiagramTranslationFrom(originalText);
-     }
+function updateTranslation()
+{
+  var originalPanel = document.querySelector(".original > textarea");
+  var originalText = originalPanel.value;
+  var translationPanel = document.querySelector(".translation > textarea");
+  translationPanel.value = actionDiagramTranslationFrom(originalText);
+}
 
 function repeat(stringToRepeat, nTimes)
-	 {
-	     var stringRepeated = "";
-	     for(var i = 0; i < nTimes; i++)
-		 {
-		     stringRepeated += stringToRepeat;
-		 }
-	     return stringRepeated;
-	 }
-	 
-     function actionDiagramTranslationFrom(text)
 {
-	 //Original shared characters
-	 const unicode_Dash = String.fromCharCode(45);
-	 const unicode_Asterisk = String.fromCharCode(42);
+  var stringRepeated = "";
+  for(var i = 0; i < nTimes; i++)
+  {
+    stringRepeated += stringToRepeat;
+  }
+  return stringRepeated;
+}
 
 
-	 // Translation shared characters
-	 const unicode_BOX_DRAWINGS_LIGHT_VERTICAL = String.fromCharCode(9474);
-	 const unicode_BOX_DRAWINGS_LIGHT_DOWN_AND_RIGHT = String.fromCharCode(9484);
-	 const unicode_BoxDrawingsLightHorizontal = String.fromCharCode(9472);
 
-	 //Original module
-	 var ORIGINAL_MODULE_HEADING = repeat(unicode_Dash, 3) + unicode_Asterisk;
-	 
-	 // Translation module
-	 var TRANSLATION_MODULE_HEADING = unicode_BOX_DRAWINGS_LIGHT_DOWN_AND_RIGHT + repeat(unicode_BoxDrawingsLightHorizontal, 3) + unicode_Asterisk;
+function actionDiagramTranslationFrom(text)
+{
+  //Original shared characters
+  const unicode_Dash = String.fromCharCode(45);
+  const unicode_Asterisk = String.fromCharCode(42);
 
 
-	 var lines = text.split("\n");
-	 var startLineBlocksBorderChars = [];
-	 var translationLines = new Array(lines.length);
-	 for(var iLine = 0; iLine < lines.length; iLine++)
-	 {
-	     
-	     console.log(lines);
-	     console.log(iLine);
-	     console.log(lines[iLine]);
-	     var originalLine = lines[iLine].toString();
+  // Translation shared characters
+  const unicode_BOX_DRAWINGS_LIGHT_VERTICAL = String.fromCharCode(9474);
+  const unicode_BOX_DRAWINGS_LIGHT_DOWN_AND_RIGHT = String.fromCharCode(9484);
+  const unicode_BoxDrawingsLightHorizontal = String.fromCharCode(9472);
 
-	     /* Create nested module and if/els blocks side borders */ 
-	     var blocksBorderDrawings = "";
-	     if(startLineBlocksBorderChars.length > 0)
-	     {
-		 blocksBorderDrawings  = startLineBlocksBorderChars.join("");
+  
+  // Original if
+  var ORIGINAL_IF_HEADING = "if";
+  // Original else
+  var ORIGINAL_ELSE_HEADING = "else";
+  //Translation if
+  var TRANSLATION_IF_HEADING = unicode_BOX_DRAWINGS_LIGHT_DOWN_AND_RIGHT + repeat(unicode_BoxDrawingsLightHorizontal, 2);
+  //Translation else
+  var unicode_BOX_DRAWINGS_LIGHT_VERTICAL_AND_RIGHT = String.fromCharCode(9500);
+  var TRANSLATION_ELSE_HEADING = unicode_BOX_DRAWINGS_LIGHT_VERTICAL_AND_RIGHT + repeat(unicode_BoxDrawingsLightHorizontal, 2);
+  
+  
+  
+  var lines = text.split("\n");
+  var startLineBlocksBorderChars = [];
+  var translationLines = new Array(lines.length);
+  for(var iLine = 0; iLine < lines.length; iLine++)
+  {
 
-	     }
+    
+    
+    var originalLine = lines[iLine].toString();
 
-	     /* Translate original module an if/else headings */
-	     var regexStringMatchingOriginalModuleHeading = ORIGINAL_MODULE_HEADING;
-	     var regex = new RegExp(regexStringMatchingOriginalModuleHeading);
-	     console.log(regexStringMatchingOriginalModuleHeading);
-	     var rawTranslatedLineText;	     
-	     if(originalLine.match(regex))
-	     {
-		  rawTranslatedLineText = originalLine.replace(ORIGINAL_MODULE_HEADING, TRANSLATION_MODULE_HEADING);
-		 startLineBlocksBorderChars.push(unicode_BOX_DRAWINGS_LIGHT_VERTICAL);
-	     }
-	     else
-	     {
-		 rawTranslatedLineText = originalLine;
-	     }
-	     
-	     console.log(rawTranslatedLineText);
-	     translationLines[iLine] = blocksBorderDrawings + rawTranslatedLineText;
+    /* Create nested module and if/els blocks side borders */ 
+    var blocksBorderDrawings = "";
+    if(startLineBlocksBorderChars.length > 0)
+    {
+      blocksBorderDrawings  = startLineBlocksBorderChars.join("");
 
-	 }
-	 
-	 return translationLines.join("\n");
+    }
+    var rawTranslatedLineText = originalLine.slice(0);
+
+    
+    /* Translate module heading */
+    // Original module heading
+    var regexStringMatchingOriginalModuleHeading = "^\\s*" + repeat(unicode_Dash, 3) + "\\" + unicode_Asterisk+ "(.*)$";
+    var regexMatchingOriginalModuleHeading = new RegExp(regexStringMatchingOriginalModuleHeading,"");
+  
+    // Translation module heading
+    var TRANSLATION_MODULE_HEADING = unicode_BOX_DRAWINGS_LIGHT_DOWN_AND_RIGHT + repeat(unicode_BoxDrawingsLightHorizontal, 3) + unicode_Asterisk;
+
+    if(regexMatchingOriginalModuleHeading.test(originalLine))
+    {
+      var regexParenthesedGroup_remainingTextOnLine = "$1";
+      var replacementString = TRANSLATION_MODULE_HEADING + regexParenthesedGroup_remainingTextOnLine;
+      rawTranslatedLineText = originalLine.replace(regexMatchingOriginalModuleHeading, replacementString);
+      startLineBlocksBorderChars.push(unicode_BOX_DRAWINGS_LIGHT_VERTICAL);
+    }
+
+    
+
+    /* Translate if/else*/
+    
+    var regexIf = new RegExp("^\\s*" + ORIGINAL_IF_HEADING);
+    if(regexIf.test(originalLine))
+    {
+      
+      var lineContentWithRemovedLeadingSpaces = originalLine.replace(/^\s*/g,"");
+      rawTranslatedLineText = TRANSLATION_IF_HEADING + lineContentWithRemovedLeadingSpaces;
+      startLineBlocksBorderChars.push(unicode_BOX_DRAWINGS_LIGHT_VERTICAL);
+    }
+    var regexElse = new RegExp("^\\s*" + ORIGINAL_ELSE_HEADING);
+    if(regexElse.test(originalLine))
+    {
+      var blocksBorderDrawingsWithoutMatchingIfBorderLineChar =  blocksBorderDrawings.slice(0, -1);
+      blocksBorderDrawings = blocksBorderDrawingsWithoutMatchingIfBorderLineChar;
+      rawTranslatedLineText = TRANSLATION_ELSE_HEADING.concat(originalLine);
+    }    
+
+
+    
+    translationLines[iLine] = blocksBorderDrawings + rawTranslatedLineText;
+  }
+  return translationLines.join("\n");
+
+}
     
 
 	 
-     }
